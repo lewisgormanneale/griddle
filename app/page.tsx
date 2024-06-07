b942223f43c8dc64ef0a59e5a5184fd3080059ae
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-06-07 15:53:33 +0100

    structure tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { generatePuzzle } from "@/utils/generatePuzzle";
 export default function Home() {
   const puzzle = generatePuzzle(5, 5);
   return (
-    <main className="flex min-h-screen flex-col items-center p-4">
+    <main className="flex flex-col justify-center items-center min-h-screen p-4 bg-orange-500">
       <Nonogram puzzle={puzzle} />
     </main>
   );
--- ui/nonogram.tsx
+++ ui/nonogram.tsx
@@ -1,16 +1,14 @@
 export function Nonogram({ puzzle }: { puzzle: number[][] }) {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="grid grid-cols-5 gap-2">
-        {puzzle.map((row, i) =>
-          row.map((cell, j) => (
-            <div
-              key={`${i}-${j}`}
-              className={`w-6 h-6 ${cell ? "bg-black" : "bg-white"}`}
-            />
-          ))
-        )}
-      </div>
-    </main>
+    <div className="grid grid-cols-5 gap-2">
+      {puzzle.map((row, i) =>
+        row.map((cell, j) => (
+          <div
+            key={`${i}-${j}`}
+            className={`w-6 h-6 ${cell ? "bg-black" : "bg-white"}`}
+          />
+        ))
+      )}
+    </div>
   );
 }
