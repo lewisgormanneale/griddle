243850e25271af4deac6d6785469059bea30c0fd
Lewis Gorman-Neale <lewis.gorman-neale@coxautoinc.com>
2024-07-19 16:42:44 +0100

    sidebar expand support

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import * as React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Home, Menu, Puzzle } from "lucide-react";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 type Menu = {
   label: string;
@@ -13,46 +16,54 @@ type Menu = {
 };
 
 export function Sidebar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleSidebar = () => setIsExpanded(!isExpanded);
   const menus: Menu[] = [
     {
       label: "home",
       name: "Home",
-      icon: <Home size={15} className="mr-2" />,
+      icon: <Home size={15} />,
       href: "/",
     },
     {
       label: "play",
       name: "Play",
-      icon: <Puzzle size={15} className="mr-2" />,
+      icon: <Puzzle size={15} />,
       href: "/nonogram",
     },
   ];
 
   return (
-    <div className="h-screen w-72 flex flex-col p-4 border-r">
+    <div
+      className={`h-screen ${
+        isExpanded ? "w-screen sm:w-64" : "w-18"
+      } flex flex-col p-2 border-r transition-all flex-shrink-0 flex-grow-0`}
+    >
       <ScrollArea className="h-full">
-        <div className="flex justify-end items-center">
-          <Button variant="ghost" size="icon">
+        <div className="flex justify-end items-center m-1">
+          <Button onClick={toggleSidebar} variant="ghost" size="icon">
             <Menu />
           </Button>
         </div>
-        <div className="flex flex-col gap-2 mt-2">
+        <div className={`flex flex-col gap-2 mt-2`}>
           {menus.map((menu) => (
             <React.Fragment key={menu.name}>
-              <div key={menu.name}>
-                <Link
-                  href={menu.href}
-                  className="flex items-center h-10 bg-white  p-4 text-xs hover:bg-primary dark:hover:bg-primary dark:bg-background dark:hover:text-background hover:text-white rounded-md"
+              <Link
+                href={menu.href}
+                className="flex items-center bg-white p-3 m-1 text-xs hover:bg-primary dark:hover:bg-primary dark:bg-background dark:hover:text-background hover:text-white rounded-md"
+              >
+                <div>{menu.icon}</div>
+                <span
+                  className={`${isExpanded ? "ml-2 leading-none" : "hidden"}`}
                 >
-                  <div className="w-6">{menu.icon}</div>
                   {menu.name}
-                </Link>
-              </div>
+                </span>
+              </Link>
             </React.Fragment>
           ))}
         </div>
       </ScrollArea>
-      <div className="flex items-center">
+      <div className="flex items-center justify-end">
         <ModeToggle />
       </div>
     </div>
