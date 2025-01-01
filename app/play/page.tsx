c8d87d6263c964626c63847a1a00c63a3124cd70
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-01-01 20:21:57 +0000

    add supabase client

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -4,9 +4,7 @@ import { Card } from "@/components/ui/card";
 export default function Play() {
   return (
     <div className="h-screen w-full flex flex-col items-center p-4">
-      <Card className="p-4">
         <Game />
-      </Card>
     </div>
   );
 }
