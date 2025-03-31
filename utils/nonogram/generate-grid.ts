2f8a8cc8f82f9e3dbc8704fe3cfc1732fbb95b3a
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-03-31 20:10:56 +0100

    update generateGrid function to use gridItemType enum

--- utils/nonogram/generate-grid.ts
+++ utils/nonogram/generate-grid.ts
@@ -1,4 +1,4 @@
-import { CellState, GridItem } from "@/types/types";
+import { CellState, GridItem, GridItemType } from "@/types/types";
 
 export const generateGrid = (
   rows: number[][],
@@ -9,17 +9,18 @@ export const generateGrid = (
   const maxRowClues = Math.max(...rowClues.map((clue) => clue.length));
   const maxColClues = Math.max(...colClues.map((clue) => clue.length));
 
-  // Build the top-left empty cells (where rows and columns meet)
   for (let r = 0; r < maxColClues; r++) {
     for (let c = 0; c < maxRowClues; c++) {
-      grid.push({ type: "empty" }); // No clues in this section
+      grid.push({
+        type: GridItemType.Empty,
+      });
     }
     for (let c = 0; c < rows[0].length; c++) {
       const clueValue = colClues[c][r - (maxColClues - colClues[c].length)];
       grid.push(
         clueValue !== undefined
-          ? { type: "clue", clueValue }
-          : { type: "empty" },
+          ? { type: GridItemType.Clue, clueValue }
+          : { type: GridItemType.Empty },
       );
     }
   }
@@ -30,13 +31,13 @@ export const generateGrid = (
       const clueValue = rowClues[r][c - (maxRowClues - rowClues[r].length)];
       grid.push(
         clueValue !== undefined
-          ? { type: "clue", clueValue }
-          : { type: "empty" },
+          ? { type: GridItemType.Clue, clueValue }
+          : { type: GridItemType.Empty },
       );
     }
     for (let c = 0; c < rows[r].length; c++) {
       grid.push({
-        type: "cell",
+        type: GridItemType.Cell,
         rowIndex: r,
         colIndex: c,
         cellState: CellState.Blank,
