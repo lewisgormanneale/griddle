1a52a696b6304cc45ad866466738aa29a007be3e
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-04-06 16:36:13 +0100

    place nonogram in a card

--- components/nonogram/control-panel.tsx
+++ components/nonogram/control-panel.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Card } from "../ui/card";
 import { Timer } from "@/components/nonogram/timer";
 
 export function ControlPanel({
@@ -17,10 +16,8 @@ export function ControlPanel({
   }, [winConditionMet]);
 
   return (
-    <Card className="w-64 flex justify-center items-center gap-3 p-3">
-      <div className="flex items-center gap-3">
-        <Timer time={time} setTime={setTime} timerActive={timerActive} />
-      </div>
-    </Card>
+    <div className="flex items-center gap-3">
+      <Timer time={time} setTime={setTime} timerActive={timerActive} />
+    </div>
   );
 }
