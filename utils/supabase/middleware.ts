cc60359f9b652e21faa04de4169482251537df61
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-05-23 20:11:18 +0100

    flip auth logic to only protect account path

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -33,11 +33,7 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
+  if (!user && request.nextUrl.pathname.startsWith("/account")) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
