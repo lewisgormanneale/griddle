b07182a555e25d726927cb0019cbad3001ab1baa
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-05-26 13:39:02 +0100

    provide DialogTitle for screen readers

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -8,7 +8,12 @@ import { cn } from "@/utils/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Sheet, SheetContent } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+} from "@/components/ui/sheet";
 import { Skeleton } from "@/components/ui/skeleton";
 import {
   Tooltip,
@@ -202,6 +207,8 @@ const Sidebar = React.forwardRef<
     if (isMobile) {
       return (
         <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
+          <SheetTitle className="hidden"></SheetTitle>
+          <SheetDescription className="hidden"></SheetDescription>
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
